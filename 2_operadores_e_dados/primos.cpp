dcbcb2f207aaddc89d721564bb5123790141bf76
InFog <junior@casoft.info>
2010-08-30 00:18:18 -0300

    Adicionados dois exemplos do capitulo 2. Tem um programinha para calcular primos, ele é bem legal.

---
+++ 2_operadores_e_dados/primos.cpp
@@ -0,0 +1,50 @@
+//      primos.cpp
+//
+//      Copyright 2010 Evaldo Junior Bento <junior@casoft.info>
+//
+//      This program is free software; you can redistribute it and/or modify
+//      it under the terms of the GNU General Public License as published by
+//      the Free Software Foundation; either version 2 of the License, or
+//      (at your option) any later version.
+//
+//      This program is distributed in the hope that it will be useful,
+//      but WITHOUT ANY WARRANTY; without even the implied warranty of
+//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//      GNU General Public License for more details.
+//
+//      You should have received a copy of the GNU General Public License
+//      along with this program; if not, write to the Free Software
+//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//      MA 02110-1301, USA.
+
+/**
+ * Este programa exibe os números primos entre 1 e 100
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+    bool primo;
+
+    for (i = 1; i <= 100; i++) {
+        primo = true;
+
+        if ((i != 1) && (i != 2)) {
+            for (j = 2; j < i; j++) {
+                if ((i % j) == 0) {
+                    primo = false;
+                }
+            }
+        }
+
+        if (primo) {
+            cout << i << "\n";
+        }
+    }
+
+    return 0;
+}
