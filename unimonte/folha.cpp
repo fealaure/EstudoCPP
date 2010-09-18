8bbf9570c2e735b5abc8789125e0a6f8ba255ba3
InFog <junior@casoft.info>
2010-09-18 14:37:49 -0300

    Adicionados comentário para facilitar a leitura do código.

--- unimonte/folha.cpp
+++ unimonte/folha.cpp
@@ -17,6 +17,33 @@
 //      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 //      MA 02110-1301, USA.
 
+/**
+ * PROBLEMA
+ *
+ * Faça um programa para o cálculo de uma folha de pagamento, sabendo que os
+ * descontos são do Imposto de Renda, que depende do salário bruto (conforme
+ * tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do
+ * Salário Bruto, mas não é descontado (é a empresa que deposita). O Salário
+ * Líquido corresponde ao Salário Bruto menos os descontos. O programa deverá
+ * pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas
+ * no mês.
+ *
+ * Desconto do IR:
+ * - Salário Bruto até 900 (inclusive) - isento
+ * - Salário Bruto até 1500 (inclusive) - desconto de 5%
+ * - Salário Bruto até 2500 (inclusive) - desconto de 10%
+ * - Salário Bruto acima de 2500 - desconto de 20%
+ *
+ * Imprima na tela as informações, dispostas conforme o exemplo abaixo.
+ * No exemplo o valor da hora é 5 e a quantidade de horas é 220.
+ *
+ * Salário Bruto: (5 * 220)  : R$ 1100,00
+ * (-) IR (5%)               : R$   55,00
+ * (-) INSS ( 10%)           : R$  110,00
+ * FGTS (11%)                : R$  121,00
+ * Total de descontos        : R$  165,00
+ * Salário Liquido           : R$  935,00
+ */
 
 #include <iostream>
 #include <stdio.h>
@@ -25,26 +52,29 @@ using namespace std;
 
 int main()
 {
+    // Definindo as variáveis
     float vl_hora, qt_horas, sl_bruto, ir, p_ir, sind, inss, fgts, sl_liquido;
 
+    // Perguntando o valor e quantidade de horas para o usuários
     printf("Digite o valor por hora: ");
     scanf("%f", &vl_hora);
     printf("Digite a quantidade de horas trabalhadas: ");
     scanf("%f", &qt_horas);
 
+    // Calculando o salário bruto
     sl_bruto = vl_hora * qt_horas;
 
     // Verificando o imposto de renda (IR)
-    p_ir = 0;
+    p_ir = 0; // Sem desconto de IR
     ir = 0;
     if (sl_bruto > 900) {
-        p_ir = 5;
+        p_ir = 5; // 5% de desconto de IR
     }
     if (sl_bruto > 1500) {
-        p_ir = 10;
+        p_ir = 10; // 10% de desconto de IR
     }
     if (sl_bruto > 2500) {
-        p_ir = 20;
+        p_ir = 20; // 20% de desconto de IR, ganha bem, mas paga mais imposto :-)
     }
 
     // Verificando se tem IR a pagar
@@ -52,14 +82,18 @@ int main()
         ir = (sl_bruto * p_ir) / 100;
     }
 
+    // Calculando os impostos
     sind = (sl_bruto * 3) / 100;
     inss = (sl_bruto * 10) / 100;
     fgts = (sl_bruto * 11) / 100;
 
+    // Deduzindo os impostos do salário bruto
     sl_liquido = sl_bruto - ir - sind - inss;
 
+    // Começando a impressão dos valores...
     printf("\n\nSalário bruto: R$ %.2f\n", sl_bruto);
 
+    // Verificando se é necessário imprimir a linha sobre o IR
     if (p_ir > 0) {
         printf("(-) IR (%.0f%%): R$ %.2f\n", p_ir, ir);
     }
--- unimonte/salario.cpp
+++ unimonte/salario.cpp
@@ -17,6 +17,29 @@
 //      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 //      MA 02110-1301, USA.
 
+/**
+ * PROBLEMA
+ *
+ * Faça um Programa que pergunte quanto você ganha por hora e o número de horas
+ * trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês,
+ * sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS
+ * e 5% para o sindicato, faça um programa que nos dê:
+ *
+ * - salário bruto.
+ * - quanto pagou ao INSS.
+ * - quanto pagou ao sindicato.
+ * - o salário líquido.
+ *
+ * calcule os descontos e o salário líquido, conforme a tabela abaixo:
+ *
+ * + Salário Bruto : R$
+ * - IR (11%) : R$
+ * - INSS (8%) : R$
+ * - Sindicato ( 5%) : R$
+ * = Salário Liquido : R$
+ *
+ * Obs.: Salário Bruto - Descontos = Salário Líquido.
+ */
 
 #include <iostream>
 #include <stdio.h>
@@ -25,21 +48,27 @@ using namespace std;
 
 int main()
 {
+    // Definindo as variáveis que serão usadas
     float vl_hora, qt_horas, sl_bruto, ir, sind, inss, sl_liquido;
 
+    // Pedindo os dados para o usuário
     printf("Digite o valor por hora: ");
     scanf("%f", &vl_hora);
     printf("Digite a quantidade de horas trabalhadas: ");
     scanf("%f", &qt_horas);
 
+    // Calculando o salário bruto
     sl_bruto = vl_hora * qt_horas;
 
+    // Agora os impostos... Bye bye dinheirinho :-(
     ir = (sl_bruto * 11) / 100;
     sind = (sl_bruto * 5) / 100;
     inss = (sl_bruto * 8) / 100;
 
+    // Deduzindo os impostos...
     sl_liquido = sl_bruto - ir - sind - inss;
 
+    // Imprimindo as informações
     printf("\n\nSalário bruto: R$ %.2f\n", sl_bruto);
     printf("IR (11%%): R$ %.2f\n", ir);
     printf("Sindicato (5%%): R$ %.2f\n", sind);
